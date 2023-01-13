5834ffb2fbddca3051d0f25a14ee55562a53cf9c
waziri123 <waziriallyamiri123@gmail.com>
2023-01-13 04:16:18 -0800

    Product

---
+++ database/migrations/2023_01_13_120647_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('vendor_id')->constrained()->onDelete('cascade');
+            $table->string('description');
+            $table->decimal('purchase_price', 8, 2);
+            $table->decimal('sale_price', 8, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
