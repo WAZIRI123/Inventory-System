8cf10a4d28947014787de48f0d04d43a127fab02
waziri123 <waziriallyamiri123@gmail.com>
2023-01-21 01:45:26 -0800

    test

--- resources/views/livewire/sales/create.blade.php
+++ resources/views/livewire/sales/create.blade.php
@@ -18,7 +18,7 @@
 
     <x-tall-crud-dialog-modal wire:model="confirmingItemCreation">
         <x-slot name="title" >
-            Add Record
+            Add Records
         </x-slot>
      
         <x-slot name="content"><div class="grid grid-cols-2 gap-8">
