6dbfdf1d6c43334606e27fc00887502e53ed2222
waziri ally amiri <74810402+WAZIRI123@users.noreply.github.com>
2023-01-24 00:37:55 +0300

    setting santum

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.defaults.withCredentials = true;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
@@ -32,4 +33,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
-// });
+// });// });
