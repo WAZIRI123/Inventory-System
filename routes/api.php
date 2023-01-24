8b527eee136bbb4f6c80ae7b6f9c89ca232dc35e
waziri123 <waziriallyamiri123@gmail.com>
2023-01-24 04:07:21 -0800

    install vue js ready to work

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
 });
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
