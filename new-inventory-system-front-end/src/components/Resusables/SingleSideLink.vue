19e91735863a09f5259c4d48fdab232da5ff65fe
waziri123 <waziriallyamiri123@gmail.com>
2023-01-30 02:04:46 -0800

    progress on making sidebar component

---
+++ new-inventory-system-front-end/src/components/Resusables/SingleSideLink.vue
@@ -0,0 +1,21 @@
+<template>
+
+         <!-- start::Menu link -->
+          <router-link :to="{ path: '/path',params: { userId: 123 } }"  class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"> 
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200"
+                    :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z">
+                    </path>
+                </svg>
+                <span class="ml-3 transition duration-200" :class="linkHover ? 'text-gray-100' : ''">
+                    Logout
+                </span></router-link>
+
+            <!-- end::Menu link -->
+
+</template>
+
+<script setup>
+
+</script>
---
+++ new-inventory-system-front-end/src/components/profile/NewPassword.vue
@@ -0,0 +1,84 @@
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                Change Password <Toast/>
+            </h2>
+            
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Update your account's profile information and email address.
+            </p>
+        </header>
+    
+        <form method="post"  class="mt-6 space-y-6" @submit.prevent="saveUser">
+
+            <div class="mt-4">
+                <!-- start::Default Input -->
+               <CustomInput label="email" v-model="user.email" />
+                <!-- end::Default Input -->
+        
+                <InputError :messages="errorMsg.email?errorMsg.email[0]:errorMsg.email"/>
+                </div>
+        
+                <div class="mt-4">
+                <!-- start::Default Input -->
+               <CustomInput label="name" v-model="user.name" />
+                <!-- end::Default Input -->
+                <InputError :messages="errorMsg.name?errorMsg.name[0]:errorMsg.name"/>
+                </div>
+                <div class="mt-4">
+                 <CustomButton title="Save" type="submit"/>
+                </div>
+        </form>
+    </section>
+    
+</template>
+<script setup>
+import { ref,watch} from 'vue';
+import store from "../../store";
+import { useRoute, useRouter } from "vue-router";
+import CustomButton from '../CustomButton.vue';
+import CustomInput from '../CustomInput.vue';
+import InputError from '../InputError.vue';
+import Toast from '../Toast.vue';
+
+let errorMsg = ref("");
+const router = useRouter();
+
+const route = useRoute();
+
+
+let user =ref({
+  email: '',
+  password: '',
+  remember: false
+});
+
+if (route.params.id) {
+store.dispatch("getUser", route.params.id);
+
+}
+watch(
+  () => store.state.user.data,
+  (newVal, oldVal) => {
+    
+    user.value = {
+      ...JSON.parse(JSON.stringify(newVal)),
+    };
+  }
+);
+const emit = defineEmits(['user:updated']);
+function saveUser() {
+  
+  store.dispatch("saveUser", { ...user.value }).then(({ data }) => {
+  
+    store.commit('showToast', `Profile has been updated successfully`)
+
+  }).catch(({response}) => {
+       errorMsg.value =response.data.errors;
+    });
+
+}
+
+
+</script>
